2466d42e9c3b9e290ebd003395cae195668d5eeb
dyadyasawa <dyadyasawa@gmail.com>
2023-12-30 16:52:33 +0300

    Инициализация класса PlayList.

--- src/playlist.py
+++ src/playlist.py
@@ -1,4 +1,19 @@
 
+from src.channel import Channel
+
+
 class PlayList:
     """  """
-    pass
+    def __init__(self, play_list_id):
+        self.play_list_id = play_list_id
+        self.channel = Channel.youtube.playlistItems().list(playlistId=self.play_list_id, part='id,snippet,contentDetails', maxResults=50).execute()
+
+    def total_duration(self):
+        pass
+
+    def show_best_video(self):
+        pass
+
+
+pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
+print(pl.channel)
