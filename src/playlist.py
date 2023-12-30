c4fa2af1e6735ac655fafcc156325798ba415085
dyadyasawa <dyadyasawa@gmail.com>
2023-12-30 17:44:35 +0300

    Дальнейшая работа над кодом.

--- src/playlist.py
+++ src/playlist.py
@@ -6,8 +6,8 @@ class PlayList:
     """  """
     def __init__(self, play_list_id):
         self.play_list_id = play_list_id
-        self.channel = Channel.youtube.playlistItems().list(playlistId=self.play_list_id, part='id,snippet,contentDetails', maxResults=50).execute()
-
+        self.channel = Channel.youtube.playlists().list(id=self.play_list_id, part='contentDetails,snippet', maxResults=50).execute()
+        self.title = self.channel['items'][0]['snippet']['title']
     def total_duration(self):
         pass
 
@@ -16,4 +16,4 @@ class PlayList:
 
 
 pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
-print(pl.channel)
+print(pl.title)
