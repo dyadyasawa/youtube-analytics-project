e190755629511023d221c5ab7947e397c798da70
dyadyasawa <dyadyasawa@gmail.com>
2023-12-31 16:43:09 +0300

    Внесение корректив в методы total_duration и show_best_video.

--- src/playlist.py
+++ src/playlist.py
@@ -23,41 +23,41 @@ class PlayList:
         self.title = self.info['items'][0]['snippet']['localized']['title']
         self.url = f'https://www.youtube.com/playlist?list={self.play_list_id}'
 
-        @property
-        def total_duration(self):
-            """ Суммарная длительность видеороликов из плейлиста """
+    @property
+    def total_duration(self):
+        """ Суммарная длительность видеороликов из плейлиста """
 
-            # Id видеороликов из плейлиста
-            video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
+        # Id видеороликов из плейлиста
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
 
-            # Статистика видео по его id
-            video_response = Channel.youtube.videos().list(part='contentDetails,statistics',
-                                                           id=','.join(video_ids)
-                                                           ).execute()
+        # Статистика видео по его id
+        video_response = Channel.youtube.videos().list(part='contentDetails,statistics',
+                                                       id=','.join(video_ids)
+                                                      ).execute()
 
-            total_time = datetime.timedelta(0)
-            for video in video_response['items']:
-                # YouTube video duration is in ISO 8601 format
-                iso_8601_duration = video['contentDetails']['duration']
-                duration = isodate.parse_duration(iso_8601_duration)
-                total_time += duration
-            return total_time
+        total_time = datetime.timedelta(0)
+        for video in video_response['items']:
+            # YouTube video duration is in ISO 8601 format
+            iso_8601_duration = video['contentDetails']['duration']
+            duration = isodate.parse_duration(iso_8601_duration)
+            total_time += duration
+        return total_time
 
-        def show_best_video(self):
-            """ Возвращает ссылку на самое популярное видео из плейлиста """
+    def show_best_video(self):
+        """ Возвращает ссылку на самое популярное видео из плейлиста """
 
-            video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
-            video_response = Channel.youtube.videos().list(part='statistics',
-                                                           id=','.join(video_ids)
-                                                           ).execute()
-            dict_ = video_response['items']
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
+        video_response = Channel.youtube.videos().list(part='statistics',
+                                                       id=','.join(video_ids)
+                                                       ).execute()
+        dict_ = video_response['items']
 
-            like_count = 0
-            vid_id = ''
-            for item in range(4):
+        like_count = 0
+        vid_id = ''
+        for item in range(4):
 
-                if int(dict_[item]['statistics']['likeCount']) >= like_count:
-                    like_count = int(dict_[item]['statistics']['likeCount'])
+            if int(dict_[item]['statistics']['likeCount']) >= like_count:
+                like_count = int(dict_[item]['statistics']['likeCount'])
 
-                    vid_id = dict_[item]['id']
-            return f'https://youtu.be/{vid_id}'
+                vid_id = dict_[item]['id']
+        return f'https://youtu.be/{vid_id}'
